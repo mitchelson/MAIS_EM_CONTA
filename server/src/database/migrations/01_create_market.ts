74b7c209d1fc3664a3551e7b5c89472c6e081849
Mitchelson <mitchelsonps@gmail.com>
2020-06-30 08:22:27 -0400

    Adjust Migrations

--- server/src/database/migrations/01_create_market.ts
+++ server/src/database/migrations/01_create_market.ts
@@ -3,11 +3,20 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
     return knex.schema.createTable('market', table => {
         table.increments('id').primary();
-        table.string('name').notNullable();
-        table.string('img').notNullable();
+        table.string('responsible').notNullable();
+        table.string('company').notNullable();
         table.string('score').notNullable();
+        table.string('img').notNullable();
+        //Data acess
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+        //Location
         table.decimal('latitude').notNullable();
         table.decimal('longitude').notNullable();
+        table.string('street').notNullable();
+        table.string('district').notNullable();
+        table.string('cep').notNullable();
+        table.decimal('number').notNullable();
         table.string('city').notNullable();
         table.string('state', 2).notNullable();
     })
